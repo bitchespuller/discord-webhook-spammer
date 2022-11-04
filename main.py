fd089a6cc2c884288d283a72098342c6a7b8594c
Baibers :) <96068067+Baiberslol@users.noreply.github.com>
2022-11-04 12:16:53 +0200

    Add files via upload
    Discord Webhook spammer works with requests
    
    **Change webhook url from the script**

---
+++ main.py
@@ -0,0 +1,29 @@
+import requests
+from colorama import *
+import time
+discord_webhook_url = "https://discord.com/api/webhooks/1036199918710308904/MsNbmLZcGDfRxc1migl2VUc86kIvggTHiEuvt0TS00VDSk6LaTKtEZylfJv-bDKRdaK4"
+
+print(f""" {Fore.RED}
+
+     ____  __  ______  _   _______  __    _       ____________       _____ ____  ___    __  _____  _____________ 
+   / __ \/ / / / __ \/ | / /  _/ |/ /   | |     / / ____/ __ )     / ___// __ \/   |  /  |/  /  |/  / ____/ __ /
+  / /_/ / /_/ / / / /  |/ // / |   /    | | /| / / __/ / __  |_____\__ \/ /_/ / /| | / /|_/ / /|_/ / __/ / /_/ /
+ / ____/ __  / /_/ / /|  // / /   |     | |/ |/ / /___/ /_/ /_____/__/ / ____/ ___ |/ /  / / /  / / /___/ _, _/ 
+/_/   /_/ /_/\____/_/ |_/___//_/|_|     |__/|__/_____/_____/     /____/_/   /_/  |_/_/  /_/_/  /_/_____/_/ |_|                                                                                                                       
+
+""")
+
+
+
+
+Message = {
+  "content": input("Message -> ")
+}
+
+print("Starting!")
+time.sleep(2)
+def spam():
+    while(True):
+        print(f'{Fore.LIGHTGREEN_EX}[+] Sent!')
+        requests.post(discord_webhook_url, data=Message)
+spam()
